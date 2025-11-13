81b4dd8b6a412ac06dde70d198584e260ed3b263
Francis Limousy <limousyf@gmail.com>
2025-11-13 13:58:18 -0500

    Initial Commit

---
+++ VisionTester/VisionTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTesterApp.swift
+//  VisionTester
+//
+//  Created by Francis LIMOUSY on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionTesterTests/VisionTesterTests.swift
@@ -0,0 +1,16 @@
+//
+//  VisionTesterTests.swift
+//  VisionTesterTests
+//
+//  Created by Francis LIMOUSY on 11/13/25.
+//
+
+import Testing
+
+struct VisionTesterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
